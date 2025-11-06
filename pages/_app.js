b8485299c53bea4e6e68d16cb78a6d5487734837
NowMeapp <nowme.app.service@gmail.com>
2025-11-07 00:16:26 +0900

    fix: redirect from / to /analyze

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 // pages/_app.js
 export default function App({ Component, pageProps }) {
    return (
-    <div style={{ backgroundColor: "#9aaeb5", minHeight: "100vh" }}>
+    <div style={{ backgroundColor: "#ffffff", minHeight: "100vh" }}>
       <Component {...pageProps} />
     </div>
   );
